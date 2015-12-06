98ce79cf28b026b90f432f49d36aa2138aa1df17
labo83 <mlapczyk@yahoo.co.uk>
2015-12-06 15:55:02 +0000

    Update Helfire-citadel-lfr.cs

--- Helfire-citadel-lfr.cs
+++ Helfire-citadel-lfr.cs
@@ -1,1 +1,21 @@
-Script to run helfire citadel in lfr dificulty
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms.VisualStyles;
+using Bots.DungeonBuddy.Attributes;
+using Bots.DungeonBuddy.Helpers;
+using Buddy.Coroutines;
+using Styx;
+using Styx.Common.Helpers;
+using Styx.CommonBot;
+using Styx.CommonBot.Coroutines;
+using Styx.Helpers;
+using Styx.Pathing;
+using Styx.WoWInternals;
+using Styx.WoWInternals.WoWObjects;
+using Vector2 = Tripper.Tools.Math.Vector2;
+
+
